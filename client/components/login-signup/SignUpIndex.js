217f4dbc25e14be0ac42620ce51bb7854ac2cf2d
SkwibHub <eidolongroups@gmail.com>
2019-01-30 13:37:33 -0800

    'Chore: Firebase is signing up user with email and PW'

---
+++ client/components/login-signup/SignUpIndex.js
@@ -0,0 +1,117 @@
+import React, {Component} from 'react'
+import {Link, withRouter} from 'react-router-dom'
+import {compose} from 'recompose'
+import {FirebaseContext, withFirebase} from '../../index'
+
+export const SignUpPage = () => (
+  <div>
+    <h1>SignUp</h1>
+    <FirebaseContext.Consumer>
+      {firebase => <SignUpForm firebase={firebase} />}
+    </FirebaseContext.Consumer>
+  </div>
+)
+
+const INITIAL_STATE = {
+  username: '',
+  email: '',
+  passwordOne: '',
+  passwordTwo: '',
+  error: null,
+  loading: true
+}
+
+class SignUpFormBase extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {...INITIAL_STATE}
+  }
+
+  onChange = event => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  onSubmit = event => {
+    event.preventDefault()
+    const {username, email, passwordOne} = this.state
+
+    this.props.firebase
+      .doCreateUserWithEmailAndPassword(email, passwordOne)
+      .then(authUser => {
+        this.setState({...INITIAL_STATE})
+        this.props.history.push('/studio')
+        console.log('USER: ', this.setState)
+      })
+      .catch(error => {
+        this.setState({error})
+      })
+  }
+
+  render() {
+    const {username, email, passwordOne, passwordTwo, error} = this.state
+
+    console.log('INIT ', withFirebase)
+
+    const isInvalid =
+      passwordOne !== passwordTwo ||
+      passwordOne === '' ||
+      email === '' ||
+      username === ''
+
+    return (
+      <div>
+        <h1>TEST!!</h1>
+        <form onSubmit={this.onSubmit}>
+          <input
+            name="username"
+            value={username}
+            onChange={this.onChange}
+            type="text"
+            placeholder="Full Name"
+          />
+          <input
+            name="email"
+            value={email}
+            onChange={this.onChange}
+            type="text"
+            placeholder="Email Address"
+          />
+          <input
+            name="passwordOne"
+            value={passwordOne}
+            onChange={this.onChange}
+            type="password"
+            placeholder="Password"
+          />
+          <input
+            name="passwordTwo"
+            value={passwordTwo}
+            onChange={this.onChange}
+            type="password"
+            placeholder="Confirm Password"
+          />
+          <button disabled={isInvalid} type="submit">
+            Sign Up
+          </button>
+
+          {error && <p>{error.message}</p>}
+        </form>
+      </div>
+    )
+  }
+}
+
+const SignUpLink = () => (
+  <p>
+    Don't have an account? <Link to="/studio">Sign Up</Link>
+  </p>
+)
+
+console.log('WITHFIREBASE? ', withFirebase)
+
+export const SignUpForm = withRouter(SignUpFormBase)
+
+// export const SignUpForm = compose(withRouter, withFirebase)(SignUpFormBase)
+
+// export {SignUpForm, SignUpLink}
--- client/index.js
+++ client/index.js
@@ -5,11 +5,28 @@ import {BrowserRouter} from 'react-router-dom'
 import store from './store'
 import App from './app'
 
+import Firebase from './firebase/Firebase.js'
+
+const FirebaseContext = React.createContext(null)
+
+console.log('CONTEXT: ', FirebaseContext)
+
+const withFirebase = Component => props => (
+  <FirebaseContext.Consumer>
+    {firebase => <Component {...props} firebase={firebase} />}
+  </FirebaseContext.Consumer>
+)
+console.log('WITH CONTEXT: ', withFirebase)
+
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <FirebaseContext.Provider value={new Firebase()}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </FirebaseContext.Provider>,
   document.getElementById('app')
 )
+
+export {FirebaseContext, withFirebase}
