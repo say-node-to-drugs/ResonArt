0f6c2cb9fe857179ad29fc38e1d41f132856ab4e
SkwibHub <eidolongroups@gmail.com>
2019-01-31 12:10:55 -0800

    'Enhancement: Added URL protection from non-Auth users.'

--- client/components/login-signup/AccountIndex.js
+++ client/components/login-signup/AccountIndex.js
@@ -2,11 +2,21 @@ import React from 'react'
 
 import {PasswordForgetForm} from './PasswordForget.js'
 import PasswordChangeForm from './PasswordChange.js'
+import {withAuthorization} from './withAuthorization.js'
+import {AuthUserContext} from './SessionContext.js'
 
-export const AccountPage = () => (
-  <div>
-    <h1>Account Page</h1>
-    <PasswordForgetForm />
-    <PasswordChangeForm />
-  </div>
+const AccountPage = () => (
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <h1>Account: {authUser.email}</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
 )
+
+const condition = authUser => !!authUser
+
+export default withAuthorization(condition)(AccountPage)
