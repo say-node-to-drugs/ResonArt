dba2e9c3bd6f7f5858d51c7b8ccf02e6687abefd
SkwibHub <eidolongroups@gmail.com>
2019-01-31 13:40:28 -0800

    'Files!'

--- server/index.js
+++ server/index.js
@@ -96,8 +96,7 @@ const createApp = () => {
 const startListening = () => {
   // start listening (and create a 'server' object representing our server)
   const server = app.listen(PORT, () =>
-    console.log(`Mixing it up on port ${PORT}`)
-  )
+    console.log(`Mixing it up on port ${PORT}`))
 }
 
 const syncDb = () => db.sync()
