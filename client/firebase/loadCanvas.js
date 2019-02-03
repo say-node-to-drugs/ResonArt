919e89f4eb005225fa0c23cabec4910935630f76
SkwibHub <eidolongroups@gmail.com>
2019-02-03 01:18:36 -0800

    Enhancement: Load images pulls user canvas data. Still need UI interface

--- client/firebase/loadCanvas.js
+++ client/firebase/loadCanvas.js
@@ -1,9 +1,19 @@
-export const loadCanvasFromFirebase = (p, canvasToLoad) => {
-  p.firebase.user(p.firebase.auth.currentUser.uid).on('value', snapshot => {
-    const userObject = snapshot.val()
-    console.log('SAVE CLICK USER: ', userObject)
-  })
+import preloadBlankCanvas from './preloadBlankCanvas.js'
 
+export const loadCanvasFromFirebase = p => {
   const userRef = p.firebase.user(p.firebase.auth.currentUser.uid)
   const userCanvas = userRef.child('canvas')
+  let sortedSnapshot = []
+
+  userCanvas.on('value', function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      var childData = childSnapshot.val()
+      sortedSnapshot.push(childData.canvas.imageData)
+    })
+  })
+
+  console.log('ALL IMAGE OBJECTS FROM USER PROFILE: ', sortedSnapshot)
+  console.log('FIRST IMAGE OBJECT FROM USER PROFILE: ', sortedSnapshot[0])
+
+  return sortedSnapshot[0]
 }
